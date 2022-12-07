c8241bf7b53eaf6a646afd6a51aa7e8dcdb4c461
Manon Brun <manonbrun0613@gmail.com>
2022-12-07 11:14:44 +0100

    Make functional read-or-not checkboxes

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 // List of all books entered
 let myLibrary=[];
 
-// Two separate lists to separate books that are read and those that are not
-let myLibraryRead=[];
-let myLibraryUnread=[];
-
 // Create a Book function
 function Book(title,author,pages,read) {
     this.title=title
     this.author=author
     this.pages=pages
     this.read=read
-    this.info=function () {
-        return(title+' by '+author+', '+pages+' pages');
-    }
+   
 }
 
 // Function to add a new book to the library
@@ -22,8 +16,8 @@ function addBookToLibrary(book) {
 }
 
 // Get the form element and the 'new book' button
-let form= document.querySelector('form');
-let newBookButton= document.querySelector('.new-book');
+let form=document.querySelector('form');
+let newBookButton=document.querySelector('.new-book');
 
 // Make the form appear when clicking 'new book'
 newBookButton.addEventListener('click', function() {
@@ -31,89 +25,144 @@ newBookButton.addEventListener('click', function() {
     newBookButton.style.display='none';
 });
 
-// Get the two different columns 'books read' and 'books to read'
-let booksRead=document.querySelector('.books-read')
-let booksNotRead=document.querySelector('.books-not-read')
-
-let switchRead=true;
-
 // Function that retrieves the information from the form and creates a neww Book object
 function createBook() {
     let title= document.getElementById('title').value;
     let author=document.getElementById('author').value;
     let pages=document.getElementById('pages').value;
     let read=document.getElementById('read');
+    // let checkbox= document.createElement('input');
+    // checkbox.type="checkbox";
+    // checkbox.name="read-or-not";
+    // checkbox.id="read-or-not";
     if (read.checked) {
-        read=true;
-        switchRead=true;
+        read='read.';
+    //     checkbox.checked=true;
     } else {
-        read=false;
-        switchRead=false;
+        read='not read.';
+    //     checkbox.checked=false;
     }
-    let newBook= new Book(title,author,pages,read);
  
+    let newBook= new Book(title,author,pages,read);
+    // newBook.checkbox=checkbox;
     addBookToLibrary(newBook);
 }
 
-// function that runs through the library and sorts the books in the right category
-// function assignLibrary() {
-//     for (let book of myLibrary) {
-//         console.log(book.read);
-//         if (book.read) {
-//             myLibraryRead.push(book.info());
-//         } else {
-//             myLibraryUnread.push(book.info());
-//         }
-//     }
-//     myLibrary=[];
-// }
 
-function checkboxBook (div) {
+Book.prototype.checkbox= function() {
     let checkbox= document.createElement('input');
     checkbox.type="checkbox";
     checkbox.name="read-or-not";
     checkbox.id="read-or-not";
-
-    let label=document.createElement('label');
-    label.htmlFor="read-or-not";
-    div.appendChild(checkbox);
-    div.appendChild(label);
-    console.log('checkbox div.read '+div.read)
-    console.log('div ' +div)
-    if (switchRead) {
+    if (this.read=='read.') {
         checkbox.checked=true;
     } else {
         checkbox.checked=false;
     }
+    this.checkbox=checkbox;
 }
 
-function checkOrNot() {
-    if (switchRead) {
-        let checkbox=document.getElementById()
+// Selects the div 'list' where the different book cards will be stored
+let listBook=document.querySelector('.list')
+
+// create array that stores all the checkboxes and their checked attributes
+let checkboxes=[];
+let checkNodeList;
+let checked=[];
+
+
+// Function that retrieves the list of checked and unchecked checkboxes
+function getCheckedState() {
+    checked=[];
+    for (let i=0;i<=myLibrary.length-1;i++) {
+        if (myLibrary[i].checkbox.checked) {
+            checked[i]=true;
+        } else {
+            checked[i]=false;
+        }
+    };
+};
+
+// changes the read values depending on checked list
+function readValues() {
+    let readValues=[];
+    let readDivs=document.querySelectorAll('.read-or-not');
+    for (let value of readDivs) {
+        readValues.push(value.textContent);
+    }
+    for (let i =0;i<=checked.length-1;i++) {
+        if (checked[i]) {
+            readDivs[i].textContent='read.';
+        } else {
+            readDivs[i].textContent='not read.';
+        }
     }
 }
 
-// display the books in the correct category
+// gets checkboxes, updates their checked state and changes the relevant read attributes
+function getCheckboxes(){
+    checked=[];
+    for (let box of checkboxes) {
+        checked.push(box.checked);
+        box.addEventListener('change', getCheckedState);
+        box.addEventListener('change',readValues);
+    };
+}
+
+
+// creates card
+Book.prototype.bookCard= function() {
+
+    let bookDiv=document.createElement('div');
+    // bookDiv.classList.add('card');
+    let bookTable=document.createElement('table');
+    // bookTable.classList.add('table');
+
+    let bookTitle=document.createElement('tr');
+    bookTitle.textContent=this.title;
+    bookTitle.classList.add('title');
+
+    let bookAuthor=document.createElement('tr');
+    bookAuthor.textContent=this.author;
+    bookAuthor.classList.add('author');
+
+    let bookPages=document.createElement('tr');
+    bookPages.textContent=this.pages+' pages,';
+    bookPages.classList.add('pages');
+
+    let bookRead=document.createElement('tr');
+    bookRead.textContent=this.read;
+    bookRead.classList.add('read-or-not');
+
+    let bookCheckboxRow=document.createElement('tr');
+    this.checkbox();
+
+    bookCheckboxRow.append(this.checkbox)
+   
+    bookTable.appendChild(bookTitle);
+    bookTable.appendChild(bookAuthor);
+    bookTable.appendChild(bookPages);
+    bookTable.appendChild(bookRead);
+    bookTable.appendChild(bookCheckboxRow);
+    bookDiv.appendChild(bookTable);
+    listBook.appendChild(bookDiv);
+
+    checkboxes.push(this.checkbox);
+
+}
+
+// function to display book
 function displayBook() {
-    let div = document.createElement('div');
-    let divContent=myLibrary[0];
-    div.textContent=divContent.info();
-    myLibrary=[];
-    if (divContent.read) {
-        booksRead.appendChild(div);
-        checkboxBook(div);
-    } else {
-        booksNotRead.appendChild(div);
-        checkboxBook(div);
-    }
+    myLibrary[myLibrary.length-1].bookCard();
 }
 
+// Function that creates the book from the form data and displays it on a card
 function sortBook() {
     createBook();
     displayBook();
-
 }
 
+// function that clears form once submitted
 function clearForm() {
     let title=document.getElementById('title');
     let author=document.getElementById('author');
@@ -127,30 +176,14 @@ function clearForm() {
 // Get the submit button
 let submitButton=document.querySelector('.submit');
 
+// display book card when submit button is clicked
 submitButton.addEventListener('click', sortBook);
-let checkboxes;
 
+// hide the form once submit button is clicked and clears the inputs
 submitButton.addEventListener('click', function () {
     form.style.display='none';
     newBookButton.style.display='block';
     clearForm();
-    checkboxes = document.querySelectorAll('#read-or-not');
-    console.log(checkboxes[0].checked);
+});
 
-    checkboxes.forEach(checkbox => {
-        checkbox.addEventListener('change', function(e) {
-            
-            if(e.target.checked) {
-                console.log(e.target)
-            }
-        })
-      });
-    
-
-})
-
-submitButton.addEventListener('click', function () {
-    newBookButton.style.display='block';
-    
-})
-
+submitButton.addEventListener('click', getCheckboxes);
